5485e041f41363081c45168ba089ac5de78b18c5
GoreToska <gorevoi_ivan@mail.ru>
2023-04-18 12:43:47 +0600

    18.04.23 lab

--- Assets/Scripts/4.1/LockPick.cs
+++ Assets/Scripts/4.1/LockPick.cs
@@ -8,15 +8,28 @@ public class LockPick : MonoBehaviour
     [SerializeField] private TMP_Text _secondSlot;
     [SerializeField] private TMP_Text _thirdSlot;
 
-    private float _timeLeft;
-    private int _first;
-    private int _second;
-    private int _third;
+    [SerializeField] private GameObject _endCanvas;
+    [SerializeField] private TMP_Text _endLabel;
 
-    private void Update()
+    private float _timeLeft = 60f;
+    private int _first = 7;
+    private int _second = 3;
+    private int _third = 5;
+
+    private bool _isPlaying = true;
+
+    private void FixedUpdate()
     {
-        _timeLeft -= Time.deltaTime;
-        _time.text = _timeLeft.ToString();
+        if (_isPlaying)
+        {
+            _timeLeft -= Time.fixedDeltaTime;
+            _time.text = Mathf.Round(_timeLeft).ToString();
+
+            if (_timeLeft <= 0)
+            {
+                EndGame(false);
+            }
+        }
     }
 
     private void UpdateSlots()
@@ -24,20 +37,72 @@ public class LockPick : MonoBehaviour
         _firstSlot.text = _first.ToString();
         _secondSlot.text = _second.ToString();
         _thirdSlot.text = _third.ToString();
+
+        if (_first == _second && _first == _third)
+        {
+            EndGame(true);
+        }
+    }
+
+    private int ValidateSlot(int slot)
+    {
+        if (slot > 10)
+            return 10;
+
+        if (slot < 0)
+            return 0;
+
+        return slot;
     }
 
     public void UseLockPick()
     {
+        _first -= 1;
+        _second += 1;
+        _third += 1;
 
+        _first = ValidateSlot(_first);
+        _second = ValidateSlot(_second);
+        _third = ValidateSlot(_third);
+
+        UpdateSlots();
     }
 
     public void UseDrill()
     {
+        _first += 1;
+        _second -= 1;
 
+        _first = ValidateSlot(_first);
+        _second = ValidateSlot(_second);
+        _third = ValidateSlot(_third);
+
+        UpdateSlots();
     }
 
     public void UseHammer()
     {
+        _first -= 1;
+        _second += 2;
+        _third -= 1;
 
+        _first = ValidateSlot(_first);
+        _second = ValidateSlot(_second);
+        _third = ValidateSlot(_third);
+
+        UpdateSlots();
+    }
+
+    private void EndGame(bool win)
+    {
+        _isPlaying = false;
+
+        if (win)
+            _endLabel.text = "Вы выиграли!";
+        else
+            _endLabel.text = "Вы проиграли!";
+
+        this.gameObject.SetActive(false);
+        _endCanvas.SetActive(true);
     }
 }
